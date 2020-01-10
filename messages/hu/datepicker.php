aa48c143ec9da952bce526aa8c80cb023e15d399
Uherkovich Péter <uherkovich.peter@pte.hu>
2020-01-10 10:09:29 +0100

    Hungarian translations

---
+++ messages/hu/datepicker.php
@@ -0,0 +1,28 @@
+<?php
+return [
+	'Go to today' => 'Ugrás a mai napra',
+	'Close the picker' => 'Dátumválasztó bezárása',
+	'Increment Minute' => 'Perc növelése',
+	'Pick Second' => 'Másodperc kiválasztása',
+	'Increment Second' => 'Másodperc növelése',
+	'Pick Minute' => 'Perc kiválasztása',
+	'Previous Month' => 'Előző hónap',
+	'Previous Century' => 'Előző évszázad',
+	'Pick Hour' => 'Óra kiválasztása',
+	'Previous Decade' => 'Előző évtized',
+	'Clear selection' => 'Dátum törlése',
+	'Next Century' => 'Következő évszázad',
+	'Select Time' => 'Idő kiválasztása',
+	'Decrement Hour' => 'Óra csökkentése',
+	'Select Decade' => 'Évtized kiválasztása',
+	'Next Year' => 'Következő év',
+	'Decrement Second' => 'Másodperc csökkentése',
+	'Decrement Minute' => 'Perc csökkentése',
+	'Toggle Period' => 'Időszak váltás',
+	'Increment Hour' => 'Óra növelése',
+	'Select Year' => 'Év kiválasztása',
+	'Select Month' => 'Hónap kiválasztása',
+	'Next Decade' => 'Következő évtized',
+	'Next Month' => 'Következő hónap',
+	'Previous Year' => 'Előző év'
+];
